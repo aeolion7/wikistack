0ab0f21aa3b85909a7f0ed1253c59fbcdec2a692
Joseph Lionarons <lionsamurai@gmail.com>
2018-11-09 11:45:24 -0500

    first switch

--- app.js
+++ app.js
@@ -2,23 +2,28 @@ const morgan = require('morgan');
 const express = require('express');
 const path = require('path');
 const layout = require('./views/layout');
-const { db } = require('./models'); // connect to database
+const models = require('./models'); // connect to database
+const PORT = 3000;
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // test that database is connected
-db.authenticate().then(() => {
+models.db.authenticate().then(() => {
   console.log('connected to the database');
 });
 
+const init = async () => {
+  await models.db.sync();
+
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+  });
+};
+
+init();
+
 app.get('/', (req, res, next) => {
   res.send(layout(''));
-});
-
-const PORT = 3000;
-
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
 });
--- models/index.js
+++ models/index.js
@@ -2,6 +2,30 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/wikistack');
 
-module.exports = {
-  db
-};
+// create a schema for page and user models
+// to define mappings between a model and a table, use .define()
+const Page = db.define('page', {
+  title: {
+    type: Sequelize.STRING
+  },
+  slug: { // url-safe version of the page title, for links
+    type: Sequelize.STRING
+  },
+  content: {
+    type: Sequelize.TEXT
+  },
+  status: {
+    type: Sequelize.ENUM('open', 'closed')
+  }
+});
+
+const User = db.define('user', {
+  name: {
+    type: Sequelize.STRING
+  },
+  email: {
+    type: Sequelize.STRING
+  }
+});
+
+module.exports = { db };
